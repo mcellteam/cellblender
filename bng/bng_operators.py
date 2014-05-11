8d98ae82b6266575374fa1a9ffa980395bc6ac72
Jose-Juan Tapia <jjtapia@gmail.com>
2014-05-11 16:30:37 -0400

    refactoring. changed the way external models are imported to conform to a common interface
    Changed the way files are imported in CellBlender. All importing code should create
    a json-file with molecule,parameter,reaction,release and output information.
    This file is read in the same way and passed to the appropiate handlers.
    bng_operators and sbml_operators now only contain code specific to the creation of
    this JSON file.

--- bng/bng_operators.py
+++ bng/bng_operators.py
@@ -3,12 +3,14 @@ import os
 import sys
 import io
 import subprocess
+import json
 
 import cellblender
 from cellblender import cellblender_properties, cellblender_operators
-from . import net
+#from . import net
 
 # We use per module class registration/unregistration
+filePath = ''
 def register():
     bpy.utils.register_module(__name__)
 
@@ -16,137 +18,10 @@ def register():
 def unregister():
     bpy.utils.unregister_module(__name__)
 
-class BNG_OT_parameter_add(bpy.types.Operator):
-    bl_idname = "bng.parameter_add"
-    bl_label = "Add Parameter"
-    bl_description = "Add imported parameters to an MCell model"
-    bl_options = {'REGISTER', 'UNDO'}
-
-    def execute(self, context):
-        mcell = context.scene.mcell
-        par_list = net.par_list
-        index = -1
-        count = 0
-        for key in sorted(par_list.keys()):
-            index += 1
-            #mcell.parameters.parameter_list.add()
-            #mcell.parameters.active_par_index = index
-            #parameter = mcell.parameters.parameter_list[
-            #    mcell.parameters.active_par_index]
-
-            par_name = par_list[key]['name']
-            par_value = par_list[key]['value']
-            par_unit = par_list[key]['unit']
-            par_type = par_list[key]['type']
-            #print ( "Adding parameter \"" + str(par_name) + "\"  =  \"" + str(par_value) + "\"  (" + str(par_unit) + ")" )
-            mcell.parameter_system.add_general_parameter_with_values ( par_name, par_value, par_unit, par_type )
-            count += 1
-        print ( "Added " + str(count) + " parameters" )
-        return {'FINISHED'}
 
 
-class BNG_OT_molecule_add(bpy.types.Operator):
-    bl_idname = "bng.molecule_add"
-    bl_label = "Add Molecule"
-    bl_description = "Add imported molecules from BNG-generated network"
-    bl_options = {'REGISTER', 'UNDO'}
-
-    def execute(self, context):
-        mcell = context.scene.mcell
-        ps = mcell.parameter_system
-        mol_list = net.mol_list
-        index = -1
-        count = 0
-        for key in sorted(mol_list.keys()):
-            index += 1
-            mcell.molecules.molecule_list.add()
-            mcell.molecules.active_mol_index = index
-            molecule = mcell.molecules.molecule_list[
-                mcell.molecules.active_mol_index]
-            #molecule.set_defaults()
-            molecule.init_properties(ps)
-
-            molecule.name = mol_list[key]['name']
-            molecule.type = mol_list[key]['type']
-            #molecule.diffusion_constant.expression = mol_list[key]['dif']
-            #molecule.diffusion_constant.param_data.label = "Diffusion Constant"
-            molecule.diffusion_constant.set_expr ( mol_list[key]['dif'], ps.panel_parameter_list )
-            #print ( "Adding molecule " + str(molecule.name) )
-            count += 1
-        print ( "Added " + str(count) + " molecules" )
-        return {'FINISHED'}
-
-
-class BNG_OT_reaction_add(bpy.types.Operator):
-    bl_idname = "bng.reaction_add"
-    bl_label = "Add Reaction"
-    bl_description = "Add imported reactions from BNG-generated network"
-    bl_options = {'REGISTER', 'UNDO'}
-
-    def execute(self, context):
-        mcell = context.scene.mcell
-        ps = mcell.parameter_system
-        rxn_list = net.rxn_list
-        index = -1
-        count = 0
-        for key in sorted(rxn_list.keys()):
-            index += 1
-            mcell.reactions.reaction_list.add()
-            mcell.reactions.active_rxn_index = index
-            reaction = mcell.reactions.reaction_list[
-                mcell.reactions.active_rxn_index]
-            #reaction.set_defaults()
-            reaction.init_properties(ps)
-            		
-            reaction.reactants = rxn_list[key]['reactants']
-            reaction.products = rxn_list[key]['products']
-            #reaction.fwd_rate.expression = rxn_list[key]['fwd_rate']
-            #reaction.fwd_rate.param_data.label = "Forward Rate"
-            reaction.fwd_rate.set_expr ( rxn_list[key]['fwd_rate'], ps.panel_parameter_list )
-            #print ( "Adding reaction  " + str(reaction.reactants) + "  ->  " + str(reaction.products) )
-            count += 1
-        print ( "Added " + str(count) + " reactions" )
-        return {'FINISHED'}
-
-
-class BNG_OT_release_site_add(bpy.types.Operator):
-    bl_idname = "bng.release_site_add"
-    bl_label = "Add Release Site"
-    bl_description = "Add imported release sites from BNG-generated network"
-    bl_options = {'REGISTER', 'UNDO'}
-
-    def execute(self, context):
-        mcell = context.scene.mcell
-        ps = mcell.parameter_system
-        rel_list = net.rel_list
-        index = -1
-        count = 0
-        for key in sorted(rel_list.keys()):
-            index += 1
-            mcell.release_sites.mol_release_list.add()
-            mcell.release_sites.active_release_index = index
-            release_site = mcell.release_sites.mol_release_list[
-                mcell.release_sites.active_release_index]
-            #release_site.set_defaults()
-            release_site.init_properties(ps)
-            
-            release_site.name = rel_list[key]['name']
-            release_site.molecule = rel_list[key]['molecule']
-            release_site.shape = rel_list[key]['shape']
-            release_site.orient = rel_list[key]['orient']
-            release_site.object_expr = rel_list[key]['object_expr']     # This may not be the best name to use for Release Shape
-            release_site.quantity_type = rel_list[key]['quantity_type']
-
-            #release_site.quantity.expression = rel_list[key]['quantity_expr']
-            release_site.quantity.set_expr ( rel_list[key]['quantity_expr'], ps.panel_parameter_list )
-
-            #cellblender_operators.check_release_molecule(context)
-            #print ( "Adding release site " + str(release_site.name) )
-            count += 1
-        print ( "Added " + str(count) + " release sites" )
-        return {'FINISHED'}
-
-
+def cleanup(filePath):
+    pass
 def execute_bionetgen(filepath,context):
     mcell = context.scene.mcell
     if mcell.cellblender_preferences.bionetgen_location_valid:
