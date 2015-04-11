46b3a611b7e77eeb2530644e7445b7f8d2b7a741
Bob Kuczewski <bobkuczewski@salk.edu>
2015-04-10 19:53:58 -0700

    Automatic update of cellblender_id.py file.

--- cellblender_id.py
+++ cellblender_id.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-cellblender_id = 'ce9542439c1c418167c04af0610cea7c3d1586c0'
-=======
-cellblender_id = '29a690d7cafdbf3bc39056a55eb80d9ab79429f9'
->>>>>>> 18849b267a0c69a6c2eca5003c55fc905768a4f6
+cellblender_id = '444a5208e773a92ecd4a15ba7e44bbd7bf14231b'
