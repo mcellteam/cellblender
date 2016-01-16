520104f3d8591cddd8d6a829e2afc46e78f2e55d
Bob Kuczewski <cnlbob@users.noreply.github.com>
2016-01-16 00:20:10 -0800

    Testing of python "MCell" stub (mcell_python.py) and minor changes.

--- libMCell/mcell_main.c
+++ libMCell/mcell_main.c
@@ -9,7 +9,9 @@ int main ( void ) {
   printf ( "Hello World from Main!!\n" );
   
   printf ( "My variable = %f\n", My_variable );
+  printf ( "5 factorial = %d\n", fact(5) );
+  printf ( "25 mod 7 = %d\n", my_mod(25,7) );
+  printf ( "Time = %s\n", get_time() );
 
   return ( 0 );
-
 }
---
+++ libMCell/mcell_python.py
@@ -0,0 +1,7 @@
+import libMCell
+
+#print ( "My variable = %f" % libMCell.My_variable )
+print ( "5 factorial = %d" % libMCell.fact(5) );
+print ( "25 mod 7 = %d" % libMCell.my_mod(25,7) );
+print ( "Time = %s" % libMCell.get_time() );
+
