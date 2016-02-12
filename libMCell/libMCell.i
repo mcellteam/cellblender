1cce8eac6eafc3a0b54b114c3b28c7f33102b79e
Bob Kuczewski <cnlbob@users.noreply.github.com>
2016-02-11 19:42:43 -0800

    New version based on examples from swig.org version 3.0.8.
    This version is basically a copy of the 3.0.8 example with file names
    changed to libMCell and mcell_main. This version does not use the
    distutils.core package or the setup.py file used in the previous
    version. That build method might be useful in the future.

---
+++ libMCell/libMCell.i
@@ -0,0 +1,9 @@
+/* File : libMCell.i */
+%module libMCell
+
+%{
+#include "libMCell.h"
+%}
+
+/* Use the original header file here */
+%include "libMCell.h"
---
+++ libMCell/mcell_main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <math.h>
+
+#include "libMCell.h"
+
+using namespace std;
+
+int main ( int argc, char *argv[] ) {
+
+  // Use a few C++ features
+  string greeting = "Hello from C++!!\n";
+  cout << greeting;
+
+  printf ( "Still need to free lots of stuff!!\n\n" );
+
+  return ( 0 );
+}
+
