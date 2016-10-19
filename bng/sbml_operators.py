b4c976709523a308b3884fb3ac619946e0de69f0
Jacob Czech <czech.jacob@gmail.com>
2016-10-19 16:23:50 -0400

    Clean up bng/sbml_operators.py

--- bng/sbml_operators.py
+++ bng/sbml_operators.py
@@ -1,16 +1,12 @@
 import bpy
 import os
-
 from . import sbml2blender
 from cellblender.cellblender_utils import get_python_path
 
-# import platform
-
-#from . import sbml2json
-#from . import sbml2json
 # We use per module class registration/unregistration
 
 filePath = ''
+
 
 def register():
     bpy.utils.register_module(__name__)
@@ -20,37 +16,20 @@ def unregister():
     bpy.utils.unregister_module(__name__)
 
 
-
-
-def execute_sbml2mcell(filepath,context):
+def execute_sbml2mcell(filepath, context):
     import subprocess
     import shutil
     mcell = context.scene.mcell
     python_path = get_python_path(mcell=mcell)
     destpath = os.path.dirname(__file__)
-    subprocess.call([python_path,destpath + '{0}sbml2json.py'.format(os.sep),'-i',filepath])
+    subprocess.call([python_path, destpath + '{0}sbml2json.py'.format(
+        os.sep), '-i', filepath])
     # execute_externally(filepath,context)
-    #TODO: If isTransformed is false a window should be shown that the model failed to load
+    #TODO: If isTransformed is false a window should be shown that the model
+    # failed to load
     return{'FINISHED'}
-
-
-# def execute_externally(filepath,context):
-#     import subprocess
-#     import shutil
-#     mcell = context.scene.mcell
-#     if mcell.cellblender_preferences.python_binary_valid:
-#         python_path = mcell.cellblender_preferences.python_binary
-#     else:
-#         python_path = shutil.which("python", mode=os.X_OK)
-#     destpath = os.path.dirname(__file__)
-#     #subprocess.call([python_path,destpath + '{0}sbml2json.py'.format(os.sep),'-i',filepath])
-#     if platform.system() == "Windows":
-#         subprocess.call([os.path.join(destpath, 'bin', 'sbml2json.exe'), '-i', filepath])
-#     else:
-#         subprocess.call([os.path.join(destpath, 'bin', 'sbml2json'), '-i', filepath])
 
 
 def execute_sbml2blender(filepath, context, addObjects=True):
     mcell = context.scene.mcell
     sbml2blender.sbml2blender(filepath, addObjects)
-
