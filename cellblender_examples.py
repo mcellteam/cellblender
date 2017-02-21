2b42fefe40369826d23ea476cf617cca2735f5c1
Jacob Czech <czech.jacob@gmail.com>
2017-02-21 16:41:32 -0500

    Add stub for examples.
    This is just a placeholder. It will eventually allow users to load
    various examples/demos (e.g. Lotka-Volterra).

---
+++ cellblender_examples.py
@@ -0,0 +1,35 @@
+import cellblender
+from . import parameter_system
+
+# blender imports
+import bpy
+from bpy.props import BoolProperty
+
+# We use per module class registration/unregistration
+def register():
+    bpy.utils.register_module(__name__)
+
+
+def unregister():
+    bpy.utils.unregister_module(__name__)
+
+
+class MCELL_PT_examples(bpy.types.Panel):
+    bl_label = "CellBlender - Examples"
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
+    bl_context = "scene"
+    bl_options = {'DEFAULT_CLOSED'}
+
+    def draw(self, context):
+        context.scene.mcell.cellblender_examples.draw_panel ( context, self )
+
+class CellBlenderExamplesPropertyGroup(bpy.types.PropertyGroup):
+
+    def draw_layout(self, context, layout):
+        mcell = context.scene.mcell
+
+        if not mcell.initialized:
+            mcell.draw_uninitialized ( layout )
+        else:
+            ps = mcell.parameter_system
