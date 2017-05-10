166de79f151a329a3e642db36f876f5c521d0acd
Tom Bartol <bartol@salk.edu>
2017-05-10 09:40:08 -0700

    Tweak makefile and swig setup for the lex/yacc mdl mesh parser.
      Hopefully this now works the same on both Linux and MacOSX.

--- io_mesh_mcell_mdl/setup.py
+++ io_mesh_mcell_mdl/setup.py
@@ -10,6 +10,7 @@ from distutils.core import setup, Extension
 mdlmesh_parser_module = Extension(name='_mdlmesh_parser',
                            sources=['mdlmesh_parser_wrap.c', 'mdlmesh_parser.c', 'mdlparse.bison.c', 'vector.c'],
                            extra_compile_args=['-O3'],
+                           extra_link_args=['-o_mdlmesh_parser.so'],
                            )
 
 setup (name = 'mdlmesh_parser',
