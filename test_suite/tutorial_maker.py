76ff97e157237064ca23e5cd72562004f032b5d0
Bob Kuczewski <bobkuczewski@salk.edu>
2018-01-15 19:47:25 -0800

    Added timer functions (from simulation code) to snapshot a series.
    This version assumes that the image contains an object with a material
    named "Material". It changes the material color for each frame and then
    takes a snapshot.
    
    This should be sufficient proof-of-concept to build a tutorial maker.

--- test_suite/tutorial_maker.py
+++ test_suite/tutorial_maker.py
@@ -10,21 +10,60 @@ import bpy
 from bpy.props import *
 
 class CREATE_OT_tutorial(bpy.types.Operator):
+    """Process actions after a delay so the screen can respond to commands"""
     bl_idname = "create.tutorial"
-    bl_label = "Create Tutorial"
-    bl_description = ("Create a Tutorial")
+    bl_label = "Create Tutorial Slides"
+    bl_options = {'REGISTER'}
+
+    _timer = None
+    _count = 0
+
+    def modal(self, context, event):
+        if event.type == 'TIMER':
+            print ( "Timer event" )
+
+            if self._count > 0:
+
+                fname = "tutorial_maker_image_" + str(self._count) + ".png"
+                print ( "Saving file: " + fname )
+                alt_context = {
+                    'region'  : None,
+                    'area'    : None,
+                    'window'  : bpy.context.window,
+                    'scene'   : bpy.context.scene,
+                    'screen'  : bpy.context.window.screen
+                }
+                bpy.ops.screen.screenshot(alt_context, filepath=fname, full=True)
+
+                bpy.data.materials['Material'].diffuse_color[0] = self._count % 2 # red
+                bpy.data.materials['Material'].diffuse_color[1] = (self._count >> 1) % 2 # green
+                bpy.data.materials['Material'].diffuse_color[2] = (self._count >> 2) % 2 # blue
+                # just a silly way of forcing a screen update. ¯\_(ツ)_/¯
+                color = context.user_preferences.themes[0].view_3d.space.gradients.high_gradient
+                color.h += 0.01
+                color.h -= 0.01
+                self._count += -1
+
+            else:
+
+                self.cancel(context)
+
+        return {'PASS_THROUGH'}
 
     def execute(self, context):
-        fname = "tutorial_maker_image.png"
-        alt_context = {
-            'region'  : None,
-            'area'    : None,
-            'window'  : bpy.context.window,
-            'scene'   : bpy.context.scene,
-            'screen'  : bpy.context.window.screen
-        }
-        bpy.ops.screen.screenshot(alt_context, filepath=fname, full=True)
-        return{'FINISHED'}
+        print ( "Inside tutorial.execute with context = " + str(context) )
+        delay = 2.0
+        print ( "Setting timer to delay of " + str(delay) )
+        wm = context.window_manager
+        self._count = 5
+        self._timer = wm.event_timer_add(delay, context.window)
+        wm.modal_handler_add(self)
+        return {'RUNNING_MODAL'}
+
+    def cancel(self, context):
+        wm = context.window_manager
+        wm.event_timer_remove(self._timer)
+
 
 class CreateTutorialPanel(bpy.types.Panel):
     bl_label = "Create Tutorial"
