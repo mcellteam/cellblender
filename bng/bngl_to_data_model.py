4916dd70a0c1f91e31709f02fea956e2afa9a56d
Ali Sinan Saglam <asinansaglam@gmail.com>
2020-05-07 15:01:19 -0400

    Forgot a os.path.split call

--- bng/bngl_to_data_model.py
+++ bng/bngl_to_data_model.py
@@ -1384,7 +1384,7 @@ def read_data_model_from_bngsim( model ):
 
 def read_data_model_from_bngl_file ( bngl_file_name ):
   try: 
-    path = os.path.dirname(__file__)
+    path = os.path.split(os.path.dirname(__file__))
     path = path[:-1]
     bngpath = path + ("extensions", "mcell", "bng2")
     path = os.path.join(*bngpath)
