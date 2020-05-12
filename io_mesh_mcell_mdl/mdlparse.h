0c86fc7970685bdcfb30e63b637a66a9161f2748
adamhusar <ahusar@salk.edu>
2020-05-12 06:38:21 -0700

    Fixes for Windows build, disabled build of sim engine limited_cpp on Win

--- io_mesh_mcell_mdl/mdlparse.h
+++ io_mesh_mcell_mdl/mdlparse.h
@@ -3,15 +3,13 @@
 
 #include <sys/types.h>
 
-typedef unsigned int u_int;
-
 #define YY_DECL int mdllex \
   (YYSTYPE * yylval_param , yyscan_t yyscanner)
 
 struct element_list {
   struct element_list *next;
-  u_int begin;
-  u_int end;
+  unsigned int begin;
+  unsigned int end;
 };
 
 
